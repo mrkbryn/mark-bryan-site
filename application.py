15ad8104c0acd34bcbf3ad99771ba7b126b84f89
Mark Bryan <mab539@cornell.edu>
2020-12-27 21:27:19 -0600

    Attempt to fix

--- application.py
+++ application.py
@@ -1,6 +1,28 @@
-from app import create_app
+from flask import Flask
 
 
-if __name__ == '__main__':
-    application = create_app()
+def say_hello(username = "World"):
+    return '<p>Hello %s!</p>\n' % username
+
+# some bits of text for the page.
+header_text = '''
+    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
+instructions = '''
+    <p><em>Hint</em>: This is a RESTful web service! Append a username
+    to the URL (for example: <code>/Thelonious</code>) to say hello to
+    someone specific.</p>\n'''
+home_link = '<p><a href="/">Back</a></p>\n'
+footer_text = '</body>\n</html>'
+
+# EB looks for an 'application' callable by default.
+application = Flask(__name__)
+
+application.add_url_rule('/', 'index', (lambda: header_text + say_hello() + instructions +
+                                                footer_text))
+
+application.add_url_rule('/<username>', 'hello', (lambda username: header_text + say_hello(username) +
+                                                                   home_link + footer_text))
+
+if __name__ == "__main__":
+    application.debug = True
     application.run()
