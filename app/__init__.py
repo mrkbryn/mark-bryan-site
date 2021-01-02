aab9e835731275fdc352bbb03319e97fd7775ae3
Mark Bryan <mab539@cornell.edu>
2021-01-02 14:49:10 -0600

    Add authentication endpoint and CSS

--- app/__init__.py
+++ app/__init__.py
@@ -24,4 +24,10 @@ def create_app(test_config=None):
     def index():
         return render_template('index.html')
 
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
     return app
